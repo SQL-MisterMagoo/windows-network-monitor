1a1626de68dd4bf0371297537cf19421ea3c3429
Mister Magoo <mister.magoo+githubsql@gmail.com>
2024-11-02 17:46:12 +0000

    Refactor code, update assets, and improve documentation
    Removed unused image files and updated project references.
    Updated LICENSE.txt with new copyright details.
    Renamed ComboBox control in MainWindow.xaml.
    Refactored MainWindow.xaml.cs for better readability and functionality.
    Updated README.md with new project name and usage instructions.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,7 +5,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-
+using System.Timers;
+using Microsoft.UI.Dispatching;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
@@ -22,20 +23,19 @@ public sealed partial class MainWindow : Window, INotifyPropertyChanged
 {
 	private const string CategoryName = "Network Interface";
 	public event PropertyChangedEventHandler PropertyChanged;
-	private ObservableCollection<string> _networks;
+	private ObservableCollection<string> _networks = [];
 	private string _selectedNetwork;
 	PerformanceCounter netSent = null!;
 	PerformanceCounter netReceived = null!;
-	private PeriodicTimer timer;
+	private DispatcherQueueTimer timer;
 	private string _upText;
-	private string _downText;
-	private LimitedQueue<DataPoint> _statsDown = new(30);
+	private string _downText = "⌛";
+	private LimitedQueue<DataPoint> _statsDown = new(40);
 	private LimitedQueue<DataPoint> _statsUp = new(30);
-	float MaximumValue => _statsDown?.MaxBy(d => d.Value)?.Value ?? 100;
-	float AverageValue => _statsDown?.Reverse().Take(3).Average(d => d.Value) ?? 100;
-	float HalfHeight => (float)itemsRepeater.ActualHeight / 2;
-	float GetMax() => (float)(MaximumValue > 0 ? MaximumValue : itemsRepeater.ActualHeight);
-	float MaxHeight() => (float)itemsRepeater.ActualHeight;
+	float MaximumValueDown => _statsDown?.MaxBy(d => d.Value)?.Value ?? 100;
+	float AverageValueDown => _statsDown?.Reverse().Take(3).Average(d => d.Value) ?? 100;
+	float GetMaximumDownValueOrDefault() => (float)(MaximumValueDown > 0 ? MaximumValueDown : itemsRepeater.ActualHeight);
+	float GetMaxHeightToBeRendered() => (float)itemsRepeater.ActualHeight;
 	DataPoint[] GridData => _statsDown.ToArray();
 	public ObservableCollection<string> Networks
 	{
@@ -90,24 +90,53 @@ public sealed partial class MainWindow : Window, INotifyPropertyChanged
 	public MainWindow()
 	{
 		this.InitializeComponent();
-		this.AppWindow.IsShownInSwitchers = false;
-		this.AppWindow.MoveInZOrderAtTop();
 		this.AppWindow.TitleBar.ExtendsContentIntoTitleBar = true;
-		this.AppWindow.SetIcon(null); // Hide the app in the taskbar
 		AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Collapsed;
+		ConfigureRenderer();
+		networkSelector.DataContext = this;
 		AppWindow.Changed += (s, e) =>
 		{
-			AppWindow.TitleBar.SetDragRectangles([new(0, 0, (int)AppWindow.Size.Width, 32)]);
+			AppWindow.TitleBar.SetDragRectangles([new(0, 0, (int)AppWindow.Size.Width, (int)AppWindow.Size.Height)]);
 		};
-		ConfigureRenderer();
-		AppWindow.Resize(new(118, 65));
-		cb1.DataContext = this;
-		Networks = [];
 		tbAverageDown.PointerPressed += (s, e) =>
 		{
-			cb1.IsDropDownOpen = true;
+			networkSelector.IsDropDownOpen = true;
 		};
-		DiscoverNetworks();
+		timer = DispatcherQueue.CreateTimer();
+		timer.Interval = TimeSpan.FromMilliseconds(250);
+		timer.Tick += Timer_Tick;
+		timer.Start();
+		AppWindow.Resize(new(210, (int)tbAverageDown.ActualHeight + 20));
+	}
+
+	private async void Timer_Tick(DispatcherQueueTimer sender, object args)
+	{
+		if (SelectedNetwork is string { Length: > 0 } network)
+		{
+			if (netReceived?.InstanceName == network)
+			{
+				float download = netReceived.NextValue();
+				_statsDown.Enqueue(new((int)download, GetMaximumDownValueOrDefault, GetMaxHeightToBeRendered));
+				tbAverageDown.Text = FormatSpeed(AverageValueDown);
+				itemsRepeater.ItemsSource = GridData;
+				itemsRepeater.UpdateLayout();
+				ConfigureRenderer();
+			}
+			else
+			{
+				netReceived = new(CategoryName, "Bytes Received/sec", network);
+			}
+			return;
+		}
+		if (_networks is null || _networks is [])
+		{
+			sender.Stop();
+			tbAverageDown.Text = "Searching...";
+			tbAverageDown.UpdateLayout();
+			await DiscoverNetworks();
+			timer.Interval = TimeSpan.FromSeconds(1);
+			sender.Start();
+		}
 	}
 
 	private void ConfigureRenderer()
@@ -123,8 +152,8 @@ public sealed partial class MainWindow : Window, INotifyPropertyChanged
 	async Task DiscoverNetworks()
 	{
 		await Task.Yield();
-		UpText = "Warming up...";
-		DownText = "Not long now...";
+		UpText = "⌛";
+		DownText = "⌛";
 		await Task.Yield();
 		if (Networks is null || Networks.Count == 0)
 		{
@@ -132,28 +161,29 @@ public sealed partial class MainWindow : Window, INotifyPropertyChanged
 			Networks = new(category.GetInstanceNames());
 		}
 	}
-	private async void cb1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+	private void cb1_SelectionChanged(object sender, SelectionChangedEventArgs e)
 	{
-		if (cb1.SelectedItem != null)
-		{
-			if (timer is PeriodicTimer t) t.Dispose();
-			netSent = new(CategoryName, "Bytes Sent/sec", cb1.SelectedItem.ToString());
-			netReceived = new(CategoryName, "Bytes Received/sec", cb1.SelectedItem.ToString());
-			timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
-			//Windows.Graphics.RectInt32 windowRect = new(AppWindow.Position.X, AppWindow.Position.Y, AppWindow.Size.Width, AppWindow.Size.Height);
-			//var screenHeight = DisplayArea.GetFromRect(windowRect, DisplayAreaFallback.Nearest).OuterBounds.Height;
-			while (await timer.WaitForNextTickAsync())
-			{
-				float upload = netSent.NextValue();
-				_statsUp.Enqueue(new((int)upload,GetMax,MaxHeight));
-				float download = netReceived.NextValue();
-				_statsDown.Enqueue(new((int)download, GetMax, MaxHeight));
-				tbAverageDown.Text = FormatSpeed(AverageValue);
-				itemsRepeater.ItemsSource = GridData;
-				itemsRepeater.UpdateLayout();
-				ConfigureRenderer();
-			}
-		}
+		SelectedNetwork = networkSelector.SelectedItem.ToString();
+		//if (networkSelector.SelectedItem != null)
+		//{
+		//	if (timer is PeriodicTimer t) t.Dispose();
+		//	netSent = new(CategoryName, "Bytes Sent/sec", networkSelector.SelectedItem.ToString());
+		//	netReceived = new(CategoryName, "Bytes Received/sec", networkSelector.SelectedItem.ToString());
+		//	timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+		//	//Windows.Graphics.RectInt32 windowRect = new(AppWindow.Position.X, AppWindow.Position.Y, AppWindow.Size.Width, AppWindow.Size.Height);
+		//	//var screenHeight = DisplayArea.GetFromRect(windowRect, DisplayAreaFallback.Nearest).OuterBounds.Height;
+		//	while (await timer.WaitForNextTickAsync())
+		//	{
+		//		float upload = netSent.NextValue();
+		//		_statsUp.Enqueue(new((int)upload,GetMaximumDownValueOrDefault,GetMaxHeightToBeRendered));
+		//		float download = netReceived.NextValue();
+		//		_statsDown.Enqueue(new((int)download, GetMaximumDownValueOrDefault, GetMaxHeightToBeRendered));
+		//		tbAverageDown.Text = FormatSpeed(AverageValueDown);
+		//		itemsRepeater.ItemsSource = GridData;
+		//		itemsRepeater.UpdateLayout();
+		//		ConfigureRenderer();
+		//	}
+		//}
 	}
 	private void OnPropertyChanged(string propertyName)
 	{
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# MMNetMon
+# Windows Network Monitor
+
+Sample WinUI 3 app that shows an "always on top" window with a crude network monitor based on performance counters.
+
+## Instructions
+
+- Click on the speed indicator to select the network interface to be monitored.
+- Drag anywhere in the window to move it
+
